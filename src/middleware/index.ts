ed4b11d0c9e90ca2449e58f4f1ccfea98b24a5ca
Rishi Kothari <itsrishikothari@gmail.com>
2020-06-29 15:35:28 -0400

    add all middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -5,12 +5,12 @@ const filterEvent = filterFn => async ({ event, next }) => {
         }
     }
 
-export const filterChannel = id => filterEvent(event => event.channel === id)
+export const filterChannel = id => (filterEvent(event => event.channel === id)) as any
 
 export const filterChannelType = (type => filterEvent(event => event.channel_type === type)) as any
 
 export const filterDM = (filterEvent(event => event.channel_type === 'im')) as any
 
-export const filterNoBotMessages = filterEvent(event => !('subtype' in event) || event.subtype !== 'bot_message')
+export const filterNoBotMessages = (filterEvent(event => !('subtype' in event) || event.subtype !== 'bot_message')) as any
 
-export const filterThreaded = (shouldBeThreaded = true) => filterEvent(event => 'thread_ts' in event === shouldBeThreaded)
+export const filterThreaded = ((shouldBeThreaded = true) => filterEvent(event => 'thread_ts' in event === shouldBeThreaded)) as any
