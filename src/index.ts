148b412ca0bc3bc01485977c50fa6bc88dfb7ab6
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-16 11:05:04 -0400

    update index

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { signing_secret, token } from './config';
+import { App } from "@slack/bolt";
+import { filterDM, filterNoBotMessages, filterChannel } from './middleware/index';
+import * as features from './features/index'
+
+const app = new App({
+    token: token,
+    signingSecret: signing_secret
+});
+
+// require('dotenv').config()
+
+(async () => {
+      // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('Troppus is running!');
+
+  for (const [ feature, handler ] of Object.entries(features)) {
+      handler(app);
+      console.log(`Feature "${feature}" has been loaded.`)
+  }
+
+//   app.action("post_approve", async ({ ack }) => {
+//       await ack();
+
+//       console.log("message approved")
+//   })
+  
+})()
