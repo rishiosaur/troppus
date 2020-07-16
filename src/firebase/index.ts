4504b4f6318b55b17d1c7270e099cf1838204c99
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-16 11:05:17 -0400

    firebase init

---
+++ src/firebase/index.ts
@@ -0,0 +1,19 @@
+import * as config from '../config';
+
+const firebase = require ("firebase")
+
+const firebaseConfig = {
+    apiKey: config.apiKey,
+    authDomain: config.authDomain,
+    databaseURL: config.databaseURL,
+    projectId: config.projectId,
+    storageBucket: config.storageBucket,
+    messagingSenderId: config.messagingSenderId,
+    appId: config.appId,
+    measurementId: config.measurementId
+  };
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+
+export const firestore = firebase.firestore();
