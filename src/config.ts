b3d89e03e7710c9baebf463d4eed468f743a66bc
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-16 11:05:56 -0400

    config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,10 @@
 export const signing_secret = process.env.signing_secret;
 export const token = process.env.token;
+export const apiKey=process.env.apiKey;
+export const authDomain=process.env.authDomain;
+export const databaseURL=process.env.databaseURL;
+export const projectId=process.env.projectId;
+export const storageBucket=process.env.storageBucket;
+export const messagingSenderId=process.env.messagingSenderId;
+export const appId=process.env.appId;
+export const measurementId=process.env.measurementId;
