35a7d0153df45a2810f6d6e138a00b10c9acd89b
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-16 11:43:42 -0400

    add func

---
+++ src/blocks/newSubmission.js
@@ -0,0 +1,61 @@
+export const newSubmission = (msg: string, to: string, hashedUid: string, id: string) => (
+    [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: ":bell: *You have a new submission!*"
+          },
+        },
+        {
+          type: "section",
+          fields: [
+            {
+              type: "mrkdwn",
+              text: `*From:*\nh${hashedUid}`,
+            },
+            {
+              type: "mrkdwn",
+              text: `*To:*\n${to}`,
+            },
+          ],
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text:
+              `> ${msg}`,
+          },
+        },
+        {
+          type: "actions",
+          elements: [
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
+                emoji: true,
+                text: "Approve :white_check_mark:",
+              },
+              style: "primary",
+              action_id: "post_approve",
+              value: id
+            },
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
+                emoji: true,
+                text: "Deny",
+              },
+              style: "danger",
+              value: "click_me_123",
+            },
+          ],
+        },
+      ]
+)
